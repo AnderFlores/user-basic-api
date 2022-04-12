8744a8bd3d8e1c10946f65c49bac3875831e8e4b
Quintana Ainhoa <quintana.131697@e.unavarra.es>
2022-04-12 21:23:19 +0200

    [19] tests in GetUserControllerTest merged with GetUserListControllerTest in one file

--- tests/app/Infrastructure/Controller/GetUserControllerTest.php
+++ tests/app/Infrastructure/Controller/GetUserControllerTest.php
@@ -64,4 +64,36 @@ class GetUserControllerTest extends TestCase
 
         $response->assertExactJson(['id' => 1, 'email' => $email]);
     }
+    public function returnEmptyUserList()
+    {
+        $this->userDataSource
+            ->expects('getUserList')
+            ->once()
+            ->andReturn(array(""));
+
+        $response = $this->get('/api/users/list');
+
+        $response->assertStatus(Response::HTTP_OK)->assertExactJson(['list'=>array("")]);
+    }
+
+    /**
+     * @test
+     */
+    public function returnHappyPathList()
+    {
+        $this->userDataSource
+            ->expects('getUserList')
+            ->once()
+            ->andReturn(
+                response()->json([
+                    'id1' => '1',
+                    'id2' => '2',
+                    'id3' => '3',
+                ], Response::HTTP_OK)
+            );
+
+        $response = $this->get('/api/users/list');
+
+        $response->assertStatus(Response::HTTP_OK)->assertExactJson(['list'=>array("")]);
+    }
 }
